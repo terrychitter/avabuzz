5605efd8cca915a4e96debf6dbba1aff87f15341
Terry Chitter <chittert@gmail.com>
2024-07-31 19:08:44 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Grid, Container, Paper } from "@mui/material";
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import abzDefaultTheme from "./theme/abz_default_theme";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <ThemeProvider theme={abzDefaultTheme}>
       <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+import React from "react";
+
+import Button from "@mui/material/Button";
+
+const Home = () => {
+  return <Button>Login</Button>;
+};
+
+export default Home;
