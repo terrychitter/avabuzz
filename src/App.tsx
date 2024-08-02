6da62a47bb373d4fcf278ffa3aab7cb643b3048b
Terry Chitter <chittert@gmail.com>
2024-08-02 22:49:10 +0200

    Add no selection

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import "./App.css";
 
 function App() {
   return (
