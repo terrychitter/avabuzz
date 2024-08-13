33419b2e589e896f0ffd872226777933708bad4d
Terry Chitter <chittert@gmail.com>
2024-08-13 18:35:13 +0200

    Add vite pwa plugin and functionality

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,21 +1,12 @@
-import usePWAInstallPrompt from "../../usePWAInstallPrompt";
-import { Button, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import Link from "../CustomComponents/CustomLink";
 import Page from "../HOC/AnimatedPage";
 
 const Home = () => {
-  const promptInstall = usePWAInstallPrompt();
   return (
     <>
       <Page>
         <Stack direction={"row"} gap={2} p={2}>
-          <Button
-            variant="contained"
-            onClick={promptInstall}
-            id="install-button"
-          >
-            Install App
-          </Button>
           <Link to="login">Login</Link>
           <Link to="profile">Profile</Link>
         </Stack>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Avabuzz",
+        short_name: "Avabuzz",
+        description: "Avabuzz is a social media platform for Avakin Life players alike.",
+        theme_color: "#171717",
+        background_color: "#171717",
+        display: "standalone",
+        icons: [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 9091,
-    host: true
+    host: true 
   }
 })
